57bae1ad6493d27433a0d905283edbb2fae2bc10
Sahil Verma <47063792+sahilv16@users.noreply.github.com>
2020-04-10 17:23:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('.sidenav').sidenav();
+});
+
+
+// function show(param_div_id) {
+//     document.getElementById('main_place').innerHTML = document.getElementById(param_div_id).innerHTML;
+// }
